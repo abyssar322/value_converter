5ba4ed0d48ff566313bcf1a63fdf0aa75d78d756
dmh3 <anton4ikkorikf@gmail.com>
2025-12-29 19:28:16 +0300

    added docx and changes cpp

--- Value_converter/Value_converter/temperature.cpp
+++ Value_converter/Value_converter/temperature.cpp
@@ -14,100 +14,104 @@ static bool is_float(const string& str) {  //Проверка на float число
 
 int tempcalculation()
 {
+    int c;
+    bool repeat = true;
     int original, needful;
     float value, result;
     string input;
 
     setlocale(LC_ALL, "Russian");
 
-    cout << "Введите исходную единицу измерения (1 - Градус Цельсия °C, 2 - Градус Фаренгейта °F, 3 - Градус Кельвина K)" << endl;
-    cin >> original;
-    if (original != 1 && original != 2 && original != 3) {
-        cout << "Неверный ввод исходной единицы измерения" << endl;
-        return 0;
-    }
+    while (repeat) {
+        cout << "Введите исходную единицу измерения (1 - Градус Цельсия °C, 2 - Градус Фаренгейта °F, 3 - Градус Кельвина K)" << endl;
+        cin >> original;
+        if (original != 1 && original != 2 && original != 3) {
+            cout << "Неверный ввод исходной единицы измерения" << endl;
+            return 0;
+        }
 
-    cout << "Введите нужную единицу измерения (1 - Градус Цельсия °C, 2 - Градус Фаренгейта °F, 3 - Градус Кельвина K)" << endl;
-    cin >> needful;
-    if (needful != 1 && needful != 2 && needful != 3) {
-        cout << "Неверный ввод нужной единицы измерения" << endl;
-        return 0;
-    }
+        cout << "Введите нужную единицу измерения (1 - Градус Цельсия °C, 2 - Градус Фаренгейта °F, 3 - Градус Кельвина K)" << endl;
+        cin >> needful;
+        if (needful != 1 && needful != 2 && needful != 3) {
+            cout << "Неверный ввод нужной единицы измерения" << endl;
+            return 0;
+        }
 
-    cout << "Введите величину" << endl;
-    cin >> input;
-    if (!is_float(input)) { //Проверка на float число
-        cout << "Неверный ввод величины, ожидалось число." << endl;
-        return 0;
-    }
-    value = stof(input);
+        cout << "Введите величину" << endl;
+        cin >> input;
+        if (!is_float(input)) { //Проверка на float число
+            cout << "Неверный ввод величины, ожидалось число." << endl;
+            return 0;
+        }
+        value = stof(input);
 
 
-    switch (original) {
-    case 1: {
-        switch (needful) {
-        case 1:
-            result = value;
-            break;
-        case 2:
-            result = (value * 1.8) + 32;
-            break;
-        case 3:
-            result = value + 273.15;
-            break;
-        default:
+        switch (original) {
+        case 1: {
+            switch (needful) {
+            case 1:
+                result = value;
+                break;
+            case 2:
+                result = (value * 1.8) + 32;
+                break;
+            case 3:
+                result = value + 273.15;
+                break;
+            default:
+                break;
+            }
             break;
         }
-        break;
-    }
-    case 2: {
-        switch (needful) {
-        case 1:
-            result = (value - 32) / 1.8;
-            break;
-        case 2:
-            result = value;
-            break;
-        case 3:
-            result = ((value - 32) / 1.8) + 273.15;
-            break;
-        default:
-            break;
-        }
-        break;
-    }
-
-    case 3: {
-        switch (needful) {
-        case 1:
-            result = value - 273.15;
-            break;
-        case 2:
-            result = ((value - 273.15) * 1.8) + 32;
-            break;
-        case 3:
-            result = value;
-            break;
-        default:
+        case 2: {
+            switch (needful) {
+            case 1:
+                result = (value - 32) / 1.8;
+                break;
+            case 2:
+                result = value;
+                break;
+            case 3:
+                result = ((value - 32) / 1.8) + 273.15;
+                break;
+            default:
+                break;
+            }
             break;
         }
-        break;
-    }
 
-    default: {
-        break;
-    }
-    }
+        case 3: {
+            switch (needful) {
+            case 1:
+                result = value - 273.15;
+                break;
+            case 2:
+                result = ((value - 273.15) * 1.8) + 32;
+                break;
+            case 3:
+                result = value;
+                break;
+            default:
+                break;
+            }
+            break;
+        }
 
-    if (needful == 1) {
-        cout << "Величина в нужной единице измерения : " << result << " °C " << endl;
-    }
-    else if (needful == 2) {
-        cout << "Величина в нужной единице измерения : " << result << " °F " << endl;
-    }
-    else if (needful == 3) {
-        cout << "Величина в нужной единице измерения : " << result << " K " << endl;
-    }
+        default: {
+            break;
+        }
+        }
 
-    return 0;
+        if (needful == 1) {
+            cout << "Величина в нужной единице измерения : " << result << " °C " << endl;
+        }
+        else if (needful == 2) {
+            cout << "Величина в нужной единице измерения : " << result << " °F " << endl;
+        }
+        else if (needful == 3) {
+            cout << "Величина в нужной единице измерения : " << result << " K " << endl;
+        }
+        cout << "Еще раз? 1 - Да "; cin >> c;
+        if (c != 1) repeat = false;
+    }
 }
--- Value_converter/Value_converter/volume.cpp
+++ Value_converter/Value_converter/volume.cpp
@@ -15,130 +15,136 @@ static bool is_float(const string& str) {  //Проверка на float число
 
 int volcalculation()
 {
+    int c;
+    bool repeat = true;
     int original, needful;
     float value, result;
     string input;
 
     setlocale(LC_ALL, "Russian");
+    
+    while (repeat) {
+        cout << "Введите исходную единицу измерения (1 - Миллилитры ml, 2 - литры l , 3 - Кубические сантиметры cm^3, 4 - Американские галлоны gal)" << endl;
+        cin >> original;
+        if (original != 1 && original != 2 && original != 3 && original != 4) {
+            cout << "Неверный ввод исходной единицы измерения" << endl;
+            return 0;
+        }
 
-    cout << "Введите исходную единицу измерения (1 - Миллилитры ml, 2 - литры l , 3 - Кубические сантиметры cm^3, 4 - Американские галлоны gal)" << endl;
-    cin >> original;
-    if (original != 1 && original != 2 && original != 3 && original != 4) {
-        cout << "Неверный ввод исходной единицы измерения" << endl;
-        return 0;
-    }
+        cout << "Введите нужную единицу измерения (1 - Миллилитры ml, 2 - литры l , 3 - Кубические сантиметры cm^3, 4 - Американские галлоны gal)" << endl;
+        cin >> needful;
+        if (needful != 1 && needful != 2 && needful != 3 && needful != 4) {
+            cout << "Неверный ввод нужной единицы измер ения" << endl;
+            return 0;
+        }
 
-    cout << "Введите нужную единицу измерения (1 - Миллилитры ml, 2 - литры l , 3 - Кубические сантиметры cm^3, 4 - Американские галлоны gal)" << endl;
-    cin >> needful;
-    if (needful != 1 && needful != 2 && needful != 3 && needful != 4) {
-        cout << "Неверный ввод нужной единицы измер ения" << endl;
-        return 0;
-    }
-
-    cout << "Введите величину" << endl;
-    cin >> input;
-    if (!is_float(input)) { //Проверка на float число
-        cout << "Неверный ввод величины, ожидалось число." << endl;
-        return 0;
-    }
-    value = stof(input);
+        cout << "Введите величину" << endl;
+        cin >> input;
+        if (!is_float(input)) { //Проверка на float число
+            cout << "Неверный ввод величины, ожидалось число." << endl;
+            return 0;
+        }
+        value = stof(input);
 
 
-    switch (original) {
-    case 1: {
-        switch (needful) {
-        case 1:
-            result = value;
-            break;
-        case 2:
-            result = value / 1000;
-            break;
-        case 3:
-            result = value;
-            break;
-        case 4:
-            result = value / 3785.41;
-            break;
-        default:
+        switch (original) {
+        case 1: {
+            switch (needful) {
+            case 1:
+                result = value;
+                break;
+            case 2:
+                result = value / 1000;
+                break;
+            case 3:
+                result = value;
+                break;
+            case 4:
+                result = value / 3785.41;
+                break;
+            default:
+                break;
+            }
             break;
         }
-        break;
-    }
-    case 2: {
-        switch (needful) {
-        case 1:
-            result = value / 1000;
-            break;
-        case 2:
-            result = value;
-            break;
-        case 3:
-            result = value * 1000;
-            break;
-        case 4:
-            result = value / 3.78541;
-            break;
-        default:
-            break;
-        }
-        break;
-    }
-
-    case 3: {
-        switch (needful) {
-        case 1:
-            result = value;
-            break;
-        case 2:
-            result = value / 1000;
-            break;
-        case 3:
-            result = value;
-            break;
-        case 4:
-            result = value / 3785.41;
-            break;
-        default:
+        case 2: {
+            switch (needful) {
+            case 1:
+                result = value / 1000;
+                break;
+            case 2:
+                result = value;
+                break;
+            case 3:
+                result = value * 1000;
+                break;
+            case 4:
+                result = value / 3.78541;
+                break;
+            default:
+                break;
+            }
             break;
         }
-        break;
-    }
 
-    case 4: {
-        switch (needful) {
-        case 1:
-            result = value * 3785.41;
-            break;
-        case 2:
-            result = value * 3.78541;
-            break;
-        case 3:
-            result = value * 3785.41;
-            break;
-        case 4:
-            result = value;
-            break;
-        default:
+        case 3: {
+            switch (needful) {
+            case 1:
+                result = value;
+                break;
+            case 2:
+                result = value / 1000;
+                break;
+            case 3:
+                result = value;
+                break;
+            case 4:
+                result = value / 3785.41;
+                break;
+            default:
+                break;
+            }
             break;
         }
-        break;
-    }
-    default: {
-        break;
-    }
-    }
 
-    if (needful == 1) {
-        cout << "Величина в нужной единице измерения : " << setprecision(3) << result << " ml " << endl;
+        case 4: {
+            switch (needful) {
+            case 1:
+                result = value * 3785.41;
+                break;
+            case 2:
+                result = value * 3.78541;
+                break;
+            case 3:
+                result = value * 3785.41;
+                break;
+            case 4:
+                result = value;
+                break;
+            default:
+                break;
+            }
+            break;
+        }
+        default: {
+            break;
+        }
+        }
+
+        if (needful == 1) {
+            cout << "Величина в нужной единице измерения : " << setprecision(3) << result << " ml " << endl;
+        }
+        else if (needful == 2) {
+            cout << "Величина в нужной единице измерения : " << setprecision(3) << result << " l " << endl;
+        }
+        else if (needful == 3) {
+            cout << "Величина в нужной единице измерения : " << setprecision(3) << result << " cm^3 " << endl;
+        }
+        else if (needful == 4) {
+            cout << "Величина в нужной единице измерения : " << setprecision(3) << result << " gal " << endl;
+        }
+
+        cout << "Еще раз? 1 - Да "; cin >> c;
+        if (c != 1) repeat = false;
     }
-    else if (needful == 2) {
-        cout << "Величина в нужной единице измерения : " << setprecision(3) << result << " l " << endl;
-    }
-    else if (needful == 3) {
-        cout << "Величина в нужной единице измерения : " << setprecision(3) << result << " cm^3 " << endl;
-    }
-    else if (needful == 4) {
-        cout << "Величина в нужной единице измерения : " << setprecision(3) << result << " gal " << endl;
-    }
-    return 0;
 }
